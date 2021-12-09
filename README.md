9672e106a62f9e9fa19433c593afccd1dc181ee5
FUJI Goro <goro@fastly.com>
2021-12-09 13:46:00 +0900

    disable authentication

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Inspired by https://github.com/yusukebe/fastly-compute-slack-command, but implemented in Rust.
 
+
+## Command URL
+
+```
+https://legally-guiding-stork.edgecompute.app/command/fastly-echo
+```
+
--- src/main.rs
+++ src/main.rs
@@ -24,41 +24,14 @@ fn main(mut req: Request) -> Result<Response, Error> {
 
     match req.get_path() {
         "/command/fastly-echo" => {
-            // Below are some common patterns for Compute@Edge services using Rust.
-            // Head to https://developer.fastly.com/learning/compute/rust/ to discover more.
-
-            // Create a new request.
-            // let mut bereq = Request::get("http://httpbin.org/headers")
-            //     .with_header("X-Custom-Header", "Welcome to Compute@Edge!")
-            //     .with_ttl(60);
-
-            // Add request headers.
-            // bereq.set_header(
-            //     "X-Another-Custom-Header",
-            //     "Recommended reading: https://developer.fastly.com/learning/compute",
-            // );
-
-            // Forward the request to a backend.
-            // let mut beresp = bereq.send("backend_name")?;
-
-            // Remove response headers.
-            // beresp.remove_header("X-Another-Custom-Header");
-
-            // Log to a Fastly endpoint.
-            // use std::io::Write;
-            // let mut endpoint = fastly::log::Endpoint::from_name("my_endpoint");
-            // writeln!(endpoint, "Hello from the edge!").unwrap();
-
-            // Send a default synthetic response.
-
-            let env = Dictionary::open("env");
-            let slack_token = env.get("SLACK_TOKEN").unwrap();
-
             let body_form = req.take_body_form::<HashMap<String, String>>().unwrap();
-            if body_form.get("token").unwrap() != &slack_token {
-                return Ok(Response::from_status(StatusCode::UNAUTHORIZED)
-                    .with_body_text_plain("Unauthorized\n"));
-            }
+
+            // let env = Dictionary::open("env");
+            // let slack_token = env.get("SLACK_TOKEN").unwrap();
+            // if body_form.get("token").unwrap() != &slack_token {
+            //     return Ok(Response::from_status(StatusCode::UNAUTHORIZED)
+            //         .with_body_text_plain("Unauthorized\n"));
+            // }
 
             let response_type = "in_channel".to_string();
             let text = body_form.get("text").unwrap().clone();
